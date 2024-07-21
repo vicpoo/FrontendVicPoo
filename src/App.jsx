5c0d748cdaf223478c132aefff39af925130b78c
romin <233324@ids.upchiapas.edu.mx>
2024-07-21 00:45:13 -0600

    formularionew

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Administrador from './pages/Administrador';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import FormularyInventary from './pages/FormularioInventary';
 import Seller from './pages/Seller';
-import Menu from './pages/Menu';
+import FormularioInventary from './pages/FormularioInventary';
 
 function App() {
   return (
@@ -14,10 +13,12 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/Login' element={<Login />} />
-          <Route path='/FormularyInventary' element={<FormularyInventary />} />
           <Route path='/Administrador' element={<Administrador />} />
           <Route path='/Seller' element={<Seller/>} />
-          <Route path='/Menu' element={<Menu/>} />
+          <Route path='/FormularioIntary' element={<FormularioInventary/>} />
+          
+
+         
           
 
         </Routes>
--- src/pages/FormularioInventary.jsx
+++ src/pages/FormularioInventary.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import CoffeeForm from '../components/organisms/CoffeeForm';
+import MenuContainer from '../components/molecules/MenuContainer';
 
-const FormularyInventary = () => {
+function FormularioInventary () {
+    const menuItems = ['Agregar', 'Editar', 'Borrar'];
+    const handleMenuClick = (item) => {
+        setShowSection(false);
+        setShowEdit(false);
+        setShowDelete(false);
+    
+        if (item === 'Agregar') {
+          setShowSection(true);
+        } else if (item === 'Editar') {
+          setShowEdit(true);
+        } else if (item === 'Borrar') {
+          setShowDelete(true);
+        }
+      };
   return (
     <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: "url('public/Imagenes/cafe09.jpg')" }}>
-
-      <CoffeeForm />
+         <div className="w-auto md:col-span-1">
+          <MenuContainer items={menuItems} onMenuClick={handleMenuClick} />
+          {showSection && (
+            <div>
+              <CoffeeForm/>
+            </div>
+          )}
+        </div>
     </div>
   );
 };
 
-export default FormularyInventary;
+export default FormularioInventary;
